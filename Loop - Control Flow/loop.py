eb72f4f6f18c9a4aa9a57eef0c5fcecf933d6238
VNATdev-ops <vuanhtuan18092004@gmail.com>
2026-04-22 15:32:13 +0700

    Conditions and loops

---
+++ Loop - Control Flow/loop.py
@@ -0,0 +1,40 @@
+# age = 22
+
+# if age >= 18:
+#     print("You are an adult")
+# else:
+#     print("You are not an adult")
+# print("--------------------------------")
+
+# score = 85
+# if score >= 90:
+#     print("You got an A")
+# elif score >= 80:
+#     print("You got a B")
+# elif score >= 70:
+#     print("You got a C")
+# else:
+#     print("You got an F")
+# print("--------------------------------")
+
+
+# for i in range(10):
+#     print(i)
+# print("--------------------------------")
+
+# for i in range(1, 10):
+#     print(i)
+# print("--------------------------------")
+
+# for i in range(1, 10, 2):
+#     print(i)
+# print("--------------------------------")
+
+# for i in range(10, 1, -1):
+#     print(i)
+# print("--------------------------------")
+
+count = 0
+while count < 10:
+    print(count)
+    count += 1    
