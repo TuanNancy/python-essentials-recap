115c762aa492dbc1df9935842aa56a17091618d6
VNATdev-ops <vuanhtuan18092004@gmail.com>
2026-04-22 14:56:03 +0700

    first commit

---
+++ Variable/variable.py
@@ -0,0 +1,13 @@
+age = 22
+name = "Tuan"
+height = 1.72
+is_student = True
+
+print(name, age, height, is_student)
+print(age + 5, "plus")
+print(age - 5, "minus")
+print(age * 5, "multiply")
+print(age / 5, "divide")
+print(age % 5, "modulo")
+print(age ** 5, "power")
+print(age // 5, "floor division")
