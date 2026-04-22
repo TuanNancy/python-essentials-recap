6b8819752cb1ca885baa7d2c913aeec177f488d3
VNATdev-ops <vuanhtuan18092004@gmail.com>
2026-04-23 00:25:24 +0700

    important methods -> done

--- Core Data Structures/list.py
+++ Core Data Structures/list.py
@@ -63,3 +63,68 @@ print("fruits[:2]:", fruits[:2])
 print("fruits[2:]:", fruits[2:])
 print("fruits[:]:", fruits[:])
 print("fruits[-2:]:", fruits[-2:])
+
+
+#6. insert(): them phan tu vao vi tri index
+print_title("insert()")
+
+fruits = ["apple", "banana", "cherry", "orange"]
+print("Before:", fruits)
+
+fruits.insert(2, "mango")
+print("After insert(2, 'mango'):", fruits)
+
+#7. extend(): them list vao list
+print_title("extend()")
+
+fruits = ["apple", "banana", "cherry", "orange"]
+print("Before:", fruits)
+
+fruits.extend(["mango", "pineapple"])
+print("After extend(['mango', 'pineapple']):", fruits)
+
+#8. index(): tra ve vi tri cua phan tu trong list
+print_title("index()")
+
+fruits = ["apple", "banana", "cherry", "orange"]
+print("List:", fruits)
+
+print("index('banana'):", fruits.index("banana"))
+print("index('cherry'):", fruits.index("cherry"))
+print("index('orange'):", fruits.index("orange"))
+
+#9. count(): dem so lan xuat hien cua phan tu trong list
+print_title("count()")
+
+fruits = ["apple", "banana", "cherry", "orange", "banana"]
+print("List:", fruits)
+
+print("count('banana'):", fruits.count("banana"))
+print("count('cherry'):", fruits.count("cherry"))
+print("count('orange'):", fruits.count("orange"))
+print("count('mango'):", fruits.count("mango"))
+print("count('pineapple'):", fruits.count("pineapple"))
+
+#10. sort(): sap xep list
+print_title("sort()")
+
+fruits = ["apple", "banana", "cherry", "orange", "banana"]
+print("List:", fruits)
+
+fruits.sort()
+print("After sort():", fruits)
+
+fruits.sort(reverse=True)
+print("After sort(reverse=True):", fruits)
+
+#11 sorted(): sap xep list va tra ve list moi
+print_title("sorted()")
+
+fruits = ["apple", "banana", "cherry", "orange", "banana"]
+print("List:", fruits)
+
+sorted_fruits = sorted(fruits)
+print("After sorted():", sorted_fruits)
+
+sorted_fruits = sorted(fruits, reverse=True)
+print("After sorted(reverse=True):", sorted_fruits)
